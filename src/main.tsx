ddce019d08899d85bd6a8a9ab655a1e46e69ae65
Osama Jaradat <osojr2017@gmail.com>
2025-02-11 23:57:30 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from "./App.tsx";
 import store from "./rtk/store.ts";
 import { Provider } from "react-redux";
 import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
+        <Analytics framework="react" />
         <App />
     </Provider>
 );
